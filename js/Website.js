6cfd756e7b41f5d352d1f1e9a42df03bf40281a7
Sunil <slysunil101@gmail.com>
2016-10-26 00:07:46 -0700

    removed unwanted UI, added temp images

--- js/Website.js
+++ js/Website.js
@@ -1,25 +1,8 @@
-<!-- Google Map Location -->
-var myCenter = new google.maps.LatLng(45.512893, -122.685386);
 
 function initialize() 
 {
-  var mapProp = 
-  {
-    center: myCenter,
-    zoom: 12,
-    scrollwheel: false,
-    draggable: false,
-    mapTypeId: google.maps.MapTypeId.ROADMAP
-  };
-
-  var map = new google.maps.Map(document.getElementById("googleMap"),mapProp);
-
-  var marker = new google.maps.Marker({position: myCenter});
-
-  marker.setMap(map);
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
 
 // Modal Image Gallery
 function displayModal(element) 
