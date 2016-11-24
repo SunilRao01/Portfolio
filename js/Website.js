d1af3ba07c371f8e15e5542d8d14e266546b7970
Sunil <slysunil101@gmail.com>
2016-11-23 16:13:12 -0800

    Added multiple images per modal

--- js/Website.js
+++ js/Website.js
@@ -1,16 +1,25 @@
 
+var imageIndex = 1;
+var imageCount = 1;
+var imagesString;
+
 // Modal Image Gallery
 function displayModal(element) 
 {
-  document.getElementById("modelImage").src = element.src;
+  // Clear previous modal images
+
+
   document.getElementById("modal_1").style.display = "block";
 
+  // Set modal title
   var titleText = document.getElementById("title");
   titleText.textContent = element.title;
 
+  // Set modal description
   var captionText = document.getElementById("caption");
   captionText.textContent = element.alt;
 
+  // Set hyperlinks
   var projectLink = document.getElementById("projectLink");
   if (element.dataset.projectLink)
   {
@@ -37,6 +46,43 @@ function displayModal(element)
     blogLink.target="_blank";
     blogLink.style.display = "block";
   }
+
+  // Set modal images
+  var modalImageCenter = document.getElementById("modalImageCenter");
+  var modalImageLeft = document.getElementById("modalImageLeft");
+  var modalImageRight = document.getElementById("modalImageRight");
+  modalImageCenter.src = "";
+  modalImageLeft.src = "";
+  modalImageRight.src = "";
+
+  imageIndex = 1;
+
+  imagesString = element.dataset.images.toString().split(",");
+  imageCount = imagesString.length;
+  console.log("Image Count: " + imageCount);
+  // Either 1 image, or 3+
+  if (imageCount == 1)
+  {
+    imageIndex = 0;
+
+    modalImageCenter.src = imagesString;
+  }
+  else
+  {
+    imageIndex = 1;
+
+    modalImageLeft.src = imagesString[imageIndex-1];
+    modalImageCenter.src = imagesString[imageIndex];
+    modalImageRight.src = imagesString[imageIndex+1];
+
+    console.log("Left Image Height: " + modalImageLeft.height);
+    console.log("Center Image Height: " + modalImageCenter.height);
+
+    var leftPosition =  (modalImageCenter.height/2) - (modalImageLeft.height/2);
+    var rightPosition = (modalImageCenter.height/2) - (modalImageRight.height/2);
+    modalImageLeft.style = "margin-top:" + leftPosition + "px";
+    modalImageRight.style = "margin-top:" + rightPosition + "px";
+  }
 }
 
 function hideModal()
@@ -46,6 +92,18 @@ function hideModal()
   document.getElementById("projectLink").style.display = "none";
   document.getElementById("blogLink").style.display = "none";
   document.getElementById("reception").style.display = "none";
+}
+
+function updateModalImage(order)
+{
+  if (order < 0)
+  {
+    
+  }
+  else
+  {
+    
+  }
 }
 
 // Used to toggle the menu on small screens when clicking on the menu button
